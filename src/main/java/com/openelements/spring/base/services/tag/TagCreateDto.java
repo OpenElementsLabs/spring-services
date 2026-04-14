71b28b27c214aa5f3857467c2b3ec66c6c3d0fb6
Hendrik Ebbers <hendrik.ebbers@open-elements.com>
2026-04-14 21:33:04 +0200

    Refactor AbstractDbBackedDataService for improved readability and consistency

--- src/main/java/com/openelements/spring/base/services/tag/TagCreateDto.java
+++
@@ -1,5 +0,0 @@
-package com.openelements.spring.base.services.tag;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record TagCreateDto(@NotBlank String name, String description, @NotBlank String color) {}
