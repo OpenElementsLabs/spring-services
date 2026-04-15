83783244ad1e9cb59eaf15655d07ddd22f19db1e
Hendrik Ebbers <hendrik.ebbers@open-elements.com>
2026-04-15 17:22:34 +0200

    Refactor TestApplicationListener and update tests for event handling

--- src/test/java/com/openelements/spring/base/data/TestApplicationListener.java
+++ src/test/java/com/openelements/spring/base/data/TestApplicationListener.java
@@ -1,59 +1,59 @@
 package com.openelements.spring.base.data;
 
 import com.openelements.spring.base.events.GenericDataEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.ApplicationScope;
+
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.event.TransactionPhase;
-import org.springframework.transaction.event.TransactionalEventListener;
-import org.springframework.web.context.annotation.ApplicationScope;
 
 @Component
 @ApplicationScope
 public class TestApplicationListener {
 
-  private final Lock lock = new ReentrantLock();
+    private final Lock lock = new ReentrantLock();
 
-  private final Condition condition = lock.newCondition();
+    private final Condition condition = lock.newCondition();
 
-  private GenericDataEvent<ForTestDto> lastEvent;
+    private GenericDataEvent<ForTestDto> lastEvent;
 
-  @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
-  public void onApplicationEvent(GenericDataEvent<ForTestDto> event) {
-    lock.lock();
-    try {
-      lastEvent = event;
-      condition.signalAll();
-    } finally {
-      lock.unlock();
-    }
-  }
-
-  public void clearEvent() {
-    lock.lock();
-    try {
-      lastEvent = null;
-    } finally {
-      lock.unlock();
-    }
-  }
-
-  public GenericDataEvent<ForTestDto> waitForNextEvent() {
-    lock.lock();
-    try {
-      if (lastEvent != null) {
-        return lastEvent;
-      } else {
+    @EventListener
+    public void onApplicationEvent(GenericDataEvent<ForTestDto> event) {
+        lock.lock();
         try {
-          condition.await();
-        } catch (InterruptedException e) {
-          throw new RuntimeException(e);
+            lastEvent = event;
+            condition.signal();
+        } finally {
+            lock.unlock();
         }
-        return lastEvent;
-      }
-    } finally {
-      lock.unlock();
     }
-  }
+
+    public void clearEvent() {
+        lock.lock();
+        try {
+            lastEvent = null;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public GenericDataEvent<ForTestDto> waitForNextEvent() {
+        lock.lock();
+        try {
+            if (lastEvent != null) {
+                return lastEvent;
+            } else {
+                try {
+                    condition.await();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                return lastEvent;
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
 }
--- src/test/java/com/openelements/spring/base/data/Tests.java
+++ src/test/java/com/openelements/spring/base/data/Tests.java
@@ -4,198 +4,207 @@ import com.openelements.spring.base.events.GenericDataEvent;
 import com.openelements.spring.base.events.OnObjectCreate;
 import com.openelements.spring.base.events.OnObjectDelete;
 import com.openelements.spring.base.events.OnObjectUpdate;
-import java.util.UUID;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Pageable;
 
+import java.util.UUID;
+
 @SpringBootTest(classes = ForTestConfig.class)
 public class Tests {
 
-  @Autowired private ForTestDataService forTestDataService;
+    @Autowired
+    private ForTestDataService forTestDataService;
 
-  @Autowired private TestApplicationListener testApplicationListener;
+    @Autowired
+    private TestApplicationListener testApplicationListener;
 
-  @Test
-  void testSaveNew() {
-    // given:
-    final ForTestDto dto = new ForTestDto(null, "Foo");
+    @BeforeEach
+    void init() {
+        forTestDataService.getAll().forEach(dto -> forTestDataService.delete(dto));
+    }
 
-    // when:
-    final ForTestDto saved = forTestDataService.save(dto);
+    @Test
+    void testSaveNew() {
+        // given:
+        final ForTestDto dto = new ForTestDto(null, "Foo");
 
-    // then:
-    Assertions.assertNotNull(saved);
-    Assertions.assertNotNull(saved.id());
-    Assertions.assertEquals("Foo", saved.name());
-  }
+        // when:
+        final ForTestDto saved = forTestDataService.save(dto);
 
-  @Test
-  void testSaveNull() {
-    Assertions.assertThrows(NullPointerException.class, () -> forTestDataService.save(null));
-  }
+        // then:
+        Assertions.assertNotNull(saved);
+        Assertions.assertNotNull(saved.id());
+        Assertions.assertEquals("Foo", saved.name());
+    }
 
-  @Test
-  void testSaveUnKnown() {
-    // given:
-    final ForTestDto dto = new ForTestDto(UUID.randomUUID(), "Foo");
+    @Test
+    void testSaveNull() {
+        Assertions.assertThrows(NullPointerException.class, () -> forTestDataService.save(null));
+    }
 
-    // when:
-    Assertions.assertThrows(IllegalArgumentException.class, () -> forTestDataService.save(dto));
-  }
+    @Test
+    void testSaveUnKnown() {
+        // given:
+        final ForTestDto dto = new ForTestDto(UUID.randomUUID(), "Foo");
 
-  @Test
-  void testSaveNewHasReallySaved() {
-    // given:
-    final ForTestDto dto = new ForTestDto(null, "Foo");
+        // when:
+        Assertions.assertThrows(IllegalArgumentException.class, () -> forTestDataService.save(dto));
+    }
 
-    // when:
-    final ForTestDto saved = forTestDataService.save(dto);
+    @Test
+    void testSaveNewHasReallySaved() {
+        // given:
+        final ForTestDto dto = new ForTestDto(null, "Foo");
 
-    // then:
-    Assertions.assertTrue(forTestDataService.existsById(saved.id()));
-    Assertions.assertTrue(forTestDataService.findById(saved.id()).isPresent());
-    Assertions.assertNotNull(forTestDataService.findAll(Pageable.unpaged()));
-    Assertions.assertFalse(forTestDataService.findAll(Pageable.unpaged()).isEmpty());
-    Assertions.assertEquals(1, forTestDataService.findAll(Pageable.unpaged()).stream().count());
-    Assertions.assertEquals(
-        saved, forTestDataService.findAll(Pageable.unpaged()).stream().findAny().orElseThrow());
-    Assertions.assertNotNull(forTestDataService.getAll());
-    Assertions.assertFalse(forTestDataService.getAll().isEmpty());
-    Assertions.assertEquals(1, forTestDataService.getAll().stream().count());
-    Assertions.assertEquals(saved, forTestDataService.getAll().stream().findAny().orElseThrow());
-  }
+        // when:
+        final ForTestDto saved = forTestDataService.save(dto);
 
-  @Test
-  void testSaveUpdate() {
-    // given:
-    final ForTestDto dto = new ForTestDto(null, "Foo");
-    final ForTestDto saved = forTestDataService.save(dto);
+        // then:
+        Assertions.assertTrue(forTestDataService.existsById(saved.id()));
+        Assertions.assertTrue(forTestDataService.findById(saved.id()).isPresent());
+        Assertions.assertNotNull(forTestDataService.findAll(Pageable.unpaged()));
+        Assertions.assertFalse(forTestDataService.findAll(Pageable.unpaged()).isEmpty());
+        Assertions.assertEquals(1, forTestDataService.findAll(Pageable.unpaged()).stream().count());
+        Assertions.assertEquals(
+                saved, forTestDataService.findAll(Pageable.unpaged()).stream().findAny().orElseThrow());
+        Assertions.assertNotNull(forTestDataService.getAll());
+        Assertions.assertFalse(forTestDataService.getAll().isEmpty());
+        Assertions.assertEquals(1, forTestDataService.getAll().stream().count());
+        Assertions.assertEquals(saved, forTestDataService.getAll().stream().findAny().orElseThrow());
+    }
 
-    // when:
-    ForTestDto updated = forTestDataService.save(new ForTestDto(saved.id(), "Bar"));
+    @Test
+    void testSaveUpdate() {
+        // given:
+        final ForTestDto dto = new ForTestDto(null, "Foo");
+        final ForTestDto saved = forTestDataService.save(dto);
 
-    Assertions.assertNotNull(updated);
-    Assertions.assertNotNull(updated.id());
-    Assertions.assertEquals(saved.id(), updated.id());
-    Assertions.assertEquals("Bar", updated.name());
-    Assertions.assertTrue(forTestDataService.existsById(updated.id()));
-    Assertions.assertTrue(forTestDataService.findById(updated.id()).isPresent());
-    Assertions.assertEquals(updated, forTestDataService.findById(updated.id()).orElseThrow());
-    Assertions.assertEquals(
-        updated, forTestDataService.findAll(Pageable.unpaged()).stream().findAny().orElseThrow());
-    Assertions.assertEquals(updated, forTestDataService.getAll().stream().findAny().orElseThrow());
-  }
+        // when:
+        ForTestDto updated = forTestDataService.save(new ForTestDto(saved.id(), "Bar"));
 
-  @Test
-  void testFindUnknown() {
-    Assertions.assertFalse(forTestDataService.existsById(UUID.randomUUID()));
-    Assertions.assertFalse(forTestDataService.findById(UUID.randomUUID()).isPresent());
-  }
+        Assertions.assertNotNull(updated);
+        Assertions.assertNotNull(updated.id());
+        Assertions.assertEquals(saved.id(), updated.id());
+        Assertions.assertEquals("Bar", updated.name());
+        Assertions.assertTrue(forTestDataService.existsById(updated.id()));
+        Assertions.assertTrue(forTestDataService.findById(updated.id()).isPresent());
+        Assertions.assertEquals(updated, forTestDataService.findById(updated.id()).orElseThrow());
+        Assertions.assertEquals(
+                updated, forTestDataService.findAll(Pageable.unpaged()).stream().findAny().orElseThrow());
+        Assertions.assertEquals(updated, forTestDataService.getAll().stream().findAny().orElseThrow());
+    }
 
-  @Test
-  void testDeleteNull() {
-    final String nullString = null;
-    final UUID nullID = null;
-    final ForTestDto nullDto = null;
+    @Test
+    void testFindUnknown() {
+        Assertions.assertFalse(forTestDataService.existsById(UUID.randomUUID()));
+        Assertions.assertFalse(forTestDataService.findById(UUID.randomUUID()).isPresent());
+    }
 
-    Assertions.assertThrows(
-        NullPointerException.class, () -> forTestDataService.delete(nullString));
-    Assertions.assertThrows(NullPointerException.class, () -> forTestDataService.delete(nullID));
-    Assertions.assertThrows(NullPointerException.class, () -> forTestDataService.delete(nullDto));
-  }
+    @Test
+    void testDeleteNull() {
+        final String nullString = null;
+        final UUID nullID = null;
+        final ForTestDto nullDto = null;
 
-  @Test
-  void testDeleteUnknown() {
-    final UUID nullID = UUID.randomUUID();
-    final ForTestDto nullDto = new ForTestDto(nullID, "Foo");
+        Assertions.assertThrows(
+                NullPointerException.class, () -> forTestDataService.delete(nullString));
+        Assertions.assertThrows(NullPointerException.class, () -> forTestDataService.delete(nullID));
+        Assertions.assertThrows(NullPointerException.class, () -> forTestDataService.delete(nullDto));
+    }
 
-    Assertions.assertThrows(
-        IllegalArgumentException.class, () -> forTestDataService.delete(nullID));
-    Assertions.assertThrows(
-        IllegalArgumentException.class, () -> forTestDataService.delete(nullDto));
-  }
+    @Test
+    void testDeleteUnknown() {
+        final UUID nullID = UUID.randomUUID();
+        final ForTestDto nullDto = new ForTestDto(nullID, "Foo");
 
-  @Test
-  void testDeleteByDto() {
-    // given:
-    final ForTestDto dto = new ForTestDto(null, "Foo");
-    final ForTestDto saved = forTestDataService.save(dto);
+        Assertions.assertThrows(
+                IllegalArgumentException.class, () -> forTestDataService.delete(nullID));
+        Assertions.assertThrows(
+                IllegalArgumentException.class, () -> forTestDataService.delete(nullDto));
+    }
 
-    // when:
-    forTestDataService.delete(saved);
+    @Test
+    void testDeleteByDto() {
+        // given:
+        final ForTestDto dto = new ForTestDto(null, "Foo");
+        final ForTestDto saved = forTestDataService.save(dto);
 
-    // then:
-    Assertions.assertFalse(forTestDataService.existsById(saved.id()));
-    Assertions.assertFalse(forTestDataService.findById(saved.id()).isPresent());
-  }
+        // when:
+        forTestDataService.delete(saved);
 
-  @Test
-  void testDeleteById() {
-    // given:
-    final ForTestDto dto = new ForTestDto(null, "Foo");
-    final ForTestDto saved = forTestDataService.save(dto);
+        // then:
+        Assertions.assertFalse(forTestDataService.existsById(saved.id()));
+        Assertions.assertFalse(forTestDataService.findById(saved.id()).isPresent());
+    }
 
-    // when:
-    forTestDataService.delete(saved.id());
+    @Test
+    void testDeleteById() {
+        // given:
+        final ForTestDto dto = new ForTestDto(null, "Foo");
+        final ForTestDto saved = forTestDataService.save(dto);
 
-    // then:
-    Assertions.assertFalse(forTestDataService.existsById(saved.id()));
-    Assertions.assertFalse(forTestDataService.findById(saved.id()).isPresent());
-  }
+        // when:
+        forTestDataService.delete(saved.id());
 
-  @Test
-  void testSaveNewTriggersEvent() {
-    // given:
-    final ForTestDto dto = new ForTestDto(null, "Foo");
-    testApplicationListener.clearEvent();
+        // then:
+        Assertions.assertFalse(forTestDataService.existsById(saved.id()));
+        Assertions.assertFalse(forTestDataService.findById(saved.id()).isPresent());
+    }
 
-    // when:
-    final ForTestDto saved = forTestDataService.save(dto);
-    GenericDataEvent<ForTestDto> forTestDtoGenericDataEvent =
-        testApplicationListener.waitForNextEvent();
+    @Test
+    void testSaveNewTriggersEvent() {
+        // given:
+        final ForTestDto dto = new ForTestDto(null, "Foo");
+        testApplicationListener.clearEvent();
 
-    // then:
-    Assertions.assertNotNull(forTestDtoGenericDataEvent);
-    Assertions.assertEquals(saved, forTestDtoGenericDataEvent.getData());
-    Assertions.assertTrue(forTestDtoGenericDataEvent instanceof OnObjectCreate<ForTestDto>);
-  }
+        // when:
+        final ForTestDto saved = forTestDataService.save(dto);
+        GenericDataEvent<ForTestDto> forTestDtoGenericDataEvent =
+                testApplicationListener.waitForNextEvent();
 
-  @Test
-  void testSaveUpdateTriggersEvent() {
-    // given:
-    final ForTestDto dto = new ForTestDto(null, "Foo");
-    final ForTestDto saved = forTestDataService.save(dto);
-    testApplicationListener.clearEvent();
+        // then:
+        Assertions.assertNotNull(forTestDtoGenericDataEvent);
+        Assertions.assertEquals(saved, forTestDtoGenericDataEvent.getData());
+        Assertions.assertTrue(forTestDtoGenericDataEvent instanceof OnObjectCreate<ForTestDto>);
+    }
 
-    // when:
-    ForTestDto updated = forTestDataService.save(new ForTestDto(saved.id(), "Bar"));
-    GenericDataEvent<ForTestDto> forTestDtoGenericDataEvent =
-        testApplicationListener.waitForNextEvent();
+    @Test
+    void testSaveUpdateTriggersEvent() {
+        // given:
+        final ForTestDto dto = new ForTestDto(null, "Foo");
+        final ForTestDto saved = forTestDataService.save(dto);
+        testApplicationListener.clearEvent();
 
-    // then:
-    Assertions.assertNotNull(forTestDtoGenericDataEvent);
-    Assertions.assertEquals(updated, forTestDtoGenericDataEvent.getData());
-    Assertions.assertTrue(forTestDtoGenericDataEvent instanceof OnObjectUpdate<ForTestDto>);
-  }
+        // when:
+        ForTestDto updated = forTestDataService.save(new ForTestDto(saved.id(), "Bar"));
+        GenericDataEvent<ForTestDto> forTestDtoGenericDataEvent =
+                testApplicationListener.waitForNextEvent();
 
-  @Test
-  void testDeleteTriggersEvent() {
-    // given:
-    final ForTestDto dto = new ForTestDto(null, "Foo");
-    final ForTestDto saved = forTestDataService.save(dto);
-    testApplicationListener.clearEvent();
+        // then:
+        Assertions.assertNotNull(forTestDtoGenericDataEvent);
+        Assertions.assertEquals(updated, forTestDtoGenericDataEvent.getData());
+        Assertions.assertTrue(forTestDtoGenericDataEvent instanceof OnObjectUpdate<ForTestDto>);
+    }
 
-    // when:
-    forTestDataService.delete(saved.id());
-    GenericDataEvent<ForTestDto> forTestDtoGenericDataEvent =
-        testApplicationListener.waitForNextEvent();
+    @Test
+    void testDeleteTriggersEvent() {
+        // given:
+        final ForTestDto dto = new ForTestDto(null, "Foo");
+        final ForTestDto saved = forTestDataService.save(dto);
+        testApplicationListener.clearEvent();
 
-    // then:
-    Assertions.assertNotNull(forTestDtoGenericDataEvent);
-    Assertions.assertEquals(saved, forTestDtoGenericDataEvent.getData());
-    Assertions.assertTrue(forTestDtoGenericDataEvent instanceof OnObjectDelete<ForTestDto>);
-  }
+        // when:
+        forTestDataService.delete(saved.id());
+        GenericDataEvent<ForTestDto> forTestDtoGenericDataEvent =
+                testApplicationListener.waitForNextEvent();
+
+        // then:
+        Assertions.assertNotNull(forTestDtoGenericDataEvent);
+        Assertions.assertEquals(saved, forTestDtoGenericDataEvent.getData());
+        Assertions.assertTrue(forTestDtoGenericDataEvent instanceof OnObjectDelete<ForTestDto>);
+    }
 }
