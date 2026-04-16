36a479cd1613540155865a9e846b9bb3e00fb40f
Hendrik Ebbers <hendrik.ebbers@open-elements.com>
2026-04-16 19:56:18 +0200

    Add Roles class with predefined user roles for security management

---
+++ src/main/java/com/openelements/spring/base/security/user/Roles.java
@@ -0,0 +1,11 @@
+package com.openelements.spring.base.security.user;
+
+/** Predefined roles. */
+public final class Roles {
+
+  /** Predefined role for administrators. */
+  public static final String ROLE_ADMIN = "ADMIN";
+
+  /** Predefined role for it administrators. */
+  public static final String ROLE_IT_ADMIN = "IT-ADMIN";
+}
