c17b0cce2de12bf8639303f286589f1b34728c08
Hendrik Ebbers <hendrik.ebbers@open-elements.com>
2026-04-28 19:13:53 +0200

    fix(pom, SecurityConfig): add compiler plugin configuration and conditional web application support

--- src/main/java/com/openelements/spring/base/security/SecurityConfig.java
+++ src/main/java/com/openelements/spring/base/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import java.util.Collection;
 import java.util.List;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
@@ -49,6 +50,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @AutoConfiguration
 @EnableAutoConfiguration
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 @Import(ApiKeyConfig.class)
 @ComponentScan
 public class SecurityConfig {
